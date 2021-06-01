9cf3b5667d06c84a1506b8a0a1b76bc1b1671af9
TurboBora <devborivoje@gmail.com>
2021-06-01 19:33:27 +0200

    Kreiran resource MoviesController, implementirane HTTP request metode, testirano Postmanom

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MoviesController;
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// index, show, store, update, delete
+Route::middleware('api')->get('/movies', [MoviesController::class, 'index']);
+Route::middleware('api')->get('/movies/{movie}', [MoviesController::class, 'show']);
+Route::middleware('api')->post('/movies', [MoviesController::class, 'store']);
+Route::middleware('api')->put('/movies/{movie}', [MoviesController::class, 'update']);
+Route::middleware('api')->delete('/movies/{movie}', [MoviesController::class, 'delete']);
